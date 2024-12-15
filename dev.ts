c144e8b43580f5298675415d4cc642fd16854473
Josh Robertson <jpsrobertson@gmail.com>
2024-12-16 10:21:30 +1100

    deployfix

--- dev.ts
+++ dev.ts
@@ -1,20 +1,26 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
+import { join } from "$std/path/mod.ts";
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
 
 // Check if we're in build mode
 if (Deno.args.includes("build")) {
-  const buildCmd = new Deno.Command(Deno.execPath(), {
+  const process = new Deno.Command("deno", {
     args: [
       "run",
       "-A",
-      "https://deno.land/x/fresh@1.5.4/src/dev/mod.ts",
+      "--no-check",
+      join(".", "main.ts"),
       "build",
     ],
+    stdout: "inherit",
+    stderr: "inherit",
   });
-  const { success } = await buildCmd.output();
-  if (!success) {
+
+  const status = await process.output();
+  
+  if (!status.success) {
     throw new Error("Build failed");
   }
 } else {
